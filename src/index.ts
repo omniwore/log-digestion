3fac0616dd20ff2103430af0b3d4515064b73465
omniwore <akayush100@gmail.com>
2023-07-23 07:14:30 +0530

    use interface instead of 2d array

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { promises } from 'dns';
 import fs from 'fs';
+
+interface formattedLogData {
+  timestamp: string;
+  endPoint: string;
+  statusCode: string;
+}
 
 async function readLogs(): Promise<string[]> {
   const dataArray: string[] = [];
@@ -19,14 +24,14 @@ async function readLogs(): Promise<string[]> {
   return dataArray;
 }
 
-async function getFormattedLogData(filesData: string[]): Promise<string[][]> {
+async function getFormattedLogData(filesData: string[]): Promise<formattedLogData[]> {
     var lineWiseData: string[] = [];
     // add line wise data from log files into array
     for(let i=0; i<filesData.length; i++){
         lineWiseData = [...lineWiseData, ...filesData[i].split("\n")];
     }
     // read each line from aggregated log data and fetch timestamp, endpoint, status into formattedLogEntries
-    var formattedLogEntries: string[][] = [];
+    var formattedLogEntries: formattedLogData[] = [];
     for(let i =0; i<lineWiseData.length; i++) {
         if(lineWiseData[i].includes('HTTP')) {
             let logLineArray: string[] = [];
@@ -34,19 +39,19 @@ async function getFormattedLogData(filesData: string[]): Promise<string[][]> {
             //example row with API call data
             // 2023-06-08 15:48 +10:00: ::ffff:172.105.184.153 - - [08/Jun/2023:05:48:10 +0000] "GET /start.html HTTP/1.1" 404 149 "-" "curl/7.54.0"
             // here the 6 the element spliited on space " " respresents timestamp, 8, 9 represent endpoint and 11th represents ststus code
-            const timestamp = logLineArray[6].substring(1,);
-            const endpoint = logLineArray[8] + " " + logLineArray[9];
-            const statusCode = logLineArray[11];
-            formattedLogEntries.push([timestamp, endpoint, statusCode]);
+            const row_timestamp = logLineArray[6].substring(1,);
+            const row_endpoint = logLineArray[8] + " " + logLineArray[9];
+            const row_statusCode = logLineArray[11];
+            formattedLogEntries.push({timestamp: row_timestamp, endPoint: row_endpoint , statusCode: row_statusCode});
         }
     }
     return formattedLogEntries
 }
 
-async function getTimeStampCountByMinutes(formattedLogEntries: string[][]) :Promise<Map<string, number>> {
+async function getTimeStampCountByMinutes(formattedLogEntries: formattedLogData[]) :Promise<Map<string, number>> {
   const timeCount: Map<string, number> = new Map();
   for(let i=0;i<formattedLogEntries.length; i++){
-      var timeValue = formattedLogEntries[i][0];
+      var timeValue = formattedLogEntries[i].timestamp;
       // removing seconds from timestamp will allow us to aggregate endpoint count by minutes 
       // 08/Jun/2023:05:48:10, here it is in format dd/m/yyyy:hh:mm:ss
       var timeInMinutes = timeValue.substring(0, timeValue.length-3);
@@ -55,19 +60,19 @@ async function getTimeStampCountByMinutes(formattedLogEntries: string[][]) :Prom
   return timeCount;
 }
 
-async function getEndPointCount(formattedLogEntries: string[][]) :Promise<Map<string, number>> {
+async function getEndPointCount(formattedLogEntries: formattedLogData[]) :Promise<Map<string, number>> {
   const endPointCount: Map<string, number> = new Map();
   for(let i=0;i<formattedLogEntries.length; i++) {
-      var endPointValue = formattedLogEntries[i][1];
+      var endPointValue = formattedLogEntries[i].endPoint;
       endPointCount.set(endPointValue, ((endPointCount.get(endPointValue) || 0) +1)) ;
   }
   return endPointCount;
 }
 
-async function getStatusCodeCount(formattedLogEntries: string[][]) :Promise<Map<string, number>> {
+async function getStatusCodeCount(formattedLogEntries: formattedLogData[]) :Promise<Map<string, number>> {
   const statusCodeCount: Map<string, number> = new Map();
   for(let i=0;i<formattedLogEntries.length; i++) {
-      var statusCode = formattedLogEntries[i][2];
+      var statusCode = formattedLogEntries[i].statusCode;
       statusCodeCount.set(statusCode, ((statusCodeCount.get(statusCode) || 0) +1)) ;
   }
   return statusCodeCount;
